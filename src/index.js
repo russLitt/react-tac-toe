ee4c33341a643aa5b40f33ca285af6ed743c9dd6
Russell Anderson <russellanderson2000@gmail.com>
2019-10-30 22:03:30 -0400

    complete tictactoe tutorial with react

--- src/index.js
+++ src/index.js
@@ -66,14 +66,15 @@ function Square(props) {
           super(props);
           this.state = {
               history: [{
-                  squares: Array(9).fill(null),
+                squares: Array(9).fill(null),
               }],
+              stepNumber: 0,
               xIsNext: true,
           };
       }
 
       handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -84,14 +85,33 @@ function Square(props) {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
       });
     }
-    
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+          const desc = move ?
+          'go to move #' + move :
+          'go to game start';
+          return (
+              <li key={move}>
+                  <button onClick={() => this.jumpTo(move)}>{desc}</button>
+              </li>
+          )
+      });
 
       let status;
       if (winner) {
@@ -111,7 +131,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
